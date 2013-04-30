8a64e5a067e1d216737dfdbf16ef1b1702650fdf
Maxim Sokhatsky <maxim@synrc.com>
2013-04-30 22:32:17 +0300

    remove logging

--- apps/web/src/index.erl
+++ apps/web/src/index.erl
@@ -5,17 +5,16 @@
 main() -> #template { file= code:priv_dir(web) ++ "/templates/index.html" }.
 title() -> <<"N2O">>.
 
-body() -> % area of http handler
+body() -> %% area of http handler
     {ok,Pid} = wf:comet(fun() -> chat_loop() end),
   [ #span { text= <<"Your chatroom name: ">> }, 
-    #textbox { id=userName, text= <<"Anonymous">>, style= <<"width: 100px;">>, next=messageTextBox },
+    #textbox { id=userName, text= <<"Anonymous">> },
     #panel { id=chatHistory, class=chat_history },
-    #textbox { id=message, style= <<"width: 70%;">>, next=sendButton },
+    #textbox { id=message },
     #button { id=sendButton, text= <<"Send">>, postback={chat,Pid}, source=[userName,message] },
     #panel { id=status } ].
 
-event({chat,Pid}) -> % area of websocket handlers
-    error_logger:info_msg("Looper Pid ~p",[Pid]),
+event({chat,Pid}) -> %% area of websocket handler
     Username = wf:q(userName),
     Message = wf:q(message),
     Terms = [ #span { text="Message sent" }, #br{} ],
@@ -26,15 +25,14 @@ event({chat,Pid}) -> % area of websocket handlers
 
 event(Event) -> error_logger:info_msg("Event: ~p", [Event]).
 
-chat_loop() -> % background worker
+chat_loop() -> %% background worker ala comet
     receive 
         {message, Username, Message} ->
             Terms = [ #span { text=Username }, ": ",
                       #span { text=Message }, #br{} ],
             wf:insert_bottom(chatHistory, Terms),
             wf:wire("$('#chatHistory').scrollTop = $('#chatHistory').scrollHeight;"),
-            wf:flush(room);
-%           wf:flush(Pid); % we flush to websocket processes
+            wf:flush(room); %% we flush to websocket process by key
         Unknown -> error_logger:info_msg("Unknown Looper Message ~p",[Unknown])
     end,
     chat_loop().
