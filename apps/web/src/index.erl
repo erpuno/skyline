f0baa3a5325c36113a064edda26f242455b20bf1
Maxim Sokhatsky <maxim@synrc.com>
2013-04-30 22:13:28 +0300

    chat sample

--- apps/web/src/index.erl
+++ apps/web/src/index.erl
@@ -5,36 +5,36 @@
 main() -> #template { file= code:priv_dir(web) ++ "/templates/index.html" }.
 title() -> <<"N2O">>.
 
-body() -> %spawn(fun() -> chat_loop() end),
+body() -> % area of http handler
+    {ok,Pid} = wf:comet(fun() -> chat_loop() end),
   [ #span { text= <<"Your chatroom name: ">> }, 
     #textbox { id=userName, text= <<"Anonymous">>, style= <<"width: 100px;">>, next=messageTextBox },
     #panel { id=chatHistory, class=chat_history },
     #textbox { id=message, style= <<"width: 70%;">>, next=sendButton },
-    #button { id=sendButton, text= <<"Send">>, postback=chat, source=[userName,message] },
+    #button { id=sendButton, text= <<"Send">>, postback={chat,Pid}, source=[userName,message] },
     #panel { id=status } ].
 
-event(chat) ->
+event({chat,Pid}) -> % area of websocket handlers
+    error_logger:info_msg("Looper Pid ~p",[Pid]),
     Username = wf:q(userName),
     Message = wf:q(message),
-            Terms = [
-                #p{},
-                #span { text="You are the only person in the chat room.", class=message }
-            ],
+    Terms = [ #span { text="Message sent" }, #br{} ],
     wf:insert_bottom(chatHistory, Terms),
-    wf:wire("$('#messageTextBox').focus(); $('#messageTextBox').select(); "),
-    ok;
+    wf:wire("$('#message').focus(); $('#message').select(); "),
+    wf:reg_pool(room),
+    Pid ! {message, Username, Message};
 
 event(Event) -> error_logger:info_msg("Event: ~p", [Event]).
 
-chat_loop() ->
+chat_loop() -> % background worker
     receive 
         {message, Username, Message} ->
-            Terms = [
-                #span { text=Username, class=username }, ": ",
-                #span { text=Message, class=message }
-            ],
+            Terms = [ #span { text=Username }, ": ",
+                      #span { text=Message }, #br{} ],
             wf:insert_bottom(chatHistory, Terms),
             wf:wire("$('#chatHistory').scrollTop = $('#chatHistory').scrollHeight;"),
-            wf:flush()
+            wf:flush(room);
+%           wf:flush(Pid); % we flush to websocket processes
+        Unknown -> error_logger:info_msg("Unknown Looper Message ~p",[Unknown])
     end,
     chat_loop().
