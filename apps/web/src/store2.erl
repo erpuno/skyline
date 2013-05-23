5ed5c890fa25b4c478ef4a55cba2a11d2563afe6
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-05-23 03:44:34 +0000

    add store2 page

---
+++ apps/web/src/store2.erl
@@ -0,0 +1,23 @@
+-module(store2).
+-compile(export_all).
+-include_lib("n2o/include/wf.hrl").
+
+title() -> [<<"Store2">>].
+
+main() ->
+  Title = wf:render(title()),
+  Body = wf:render(body()),
+  [#dtl{file="store2", bindings=[{title,Title},{body, Body}]}].
+
+body() -> [
+  #panel{class=[navbar, "navbar-inverse", "navbar-fixed-top"], body=[
+    #panel{class=["navbar-inner"], body=[
+      #panel{class=[container], body=[
+        #link{class=[brand], url="#", text="iGratch.com" }
+      ]}
+    ]}
+  ]}
+  ].
+
+event(init) -> [];
+event(Event) -> error_logger:info_msg("Page event: ~p", [Event]),ok.
