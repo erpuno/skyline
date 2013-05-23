560065c1197912a2b14c7d2a0db07219cad92b45
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-05-23 20:31:21 +0000

    add thumbnails sample

--- apps/web/src/store2.erl
+++ apps/web/src/store2.erl
@@ -29,8 +29,60 @@ body() -> [
                   #li{body=#link{body=[#i{class=["icon-cog", "fui-gear"]},  " Preferences"]}},
                   #li{body=#link{body=[#i{class=["icon-off"]}, " Logout" ]}}
                 ]} ]} ]} ]} ]} ]} ]} ]},
-    #panel{body=["Content"]}
-  ].
+
+  #panel{class=[container],body=[
+    #panel{class=[row], body=[
+      #list{class=[thumbnails], body=[
+        #li{body=[
+          #panel{style="margin-left:20px;", class=[thumbnail], body=[
+            #list{class=[breadcrumb], body=[
+              #li{style="font-size:24.5px;font-weight:bold;", body=[#link{text="Action"}, #span{class=["divider"], body=["/"]}]},
+              #li{style="font-size:24.5px", body=#link{text="Crysis 3"}}
+            ]},
+            #image{image="/static/img/p1.jpg"},
+            #h3{style="display:inline-block;",text="Developer:"},
+            #link{style="font-size:24.5px", text="Electronic Arts", url="#"},
+            #p{class=[lead],body=["The award-winning developer Crytek is back with Crysis 3, the first blockbuster shooter of 2013!"]},
+            #panel{class=["row offset4"], body=[
+              #panel{class=["btn-group"], body=#button{class=["btn btn-warning btn-large"], text="Buy It!"}},
+              #panel{class=["btn-group"], body=#button{class=["btn btn-info btn-large"], text="Review"}}
+            ]}
+          ]}
+        ]},
+        #li{body=[
+          #panel{style="margin-left:20px;", class=[thumbnail], body=[
+            #list{class=[breadcrumb], body=[
+              #li{style="font-size:24.5px;font-weight:bold;", body=[#link{text="Action"}, #span{class=["divider"], body=["/"]}]},
+              #li{style="font-size:24.5px", body=#link{text="Battlefield 4"}}
+            ]},
+            #image{image="/static/img/p2.jpg"},
+            #h3{style="display:inline-block;",text="Developer:"},
+            #link{style="font-size:24.5px", text="Electronic Arts", url="#"},
+            #p{class=[lead],body=["The award-winning developer Crytek is back with Crysis 3, the first blockbuster shooter of 2013!"]},
+            #panel{class=["row offset4"], body=[
+              #panel{class=["btn-group"], body=#button{class=["btn btn-warning btn-large"], text="Buy It!"}},
+              #panel{class=["btn-group"], body=#button{class=["btn btn-info btn-large"], text="Review"}}
+            ]}
+          ]}
+        ]}
+      ]}
+    ]},
+
+    #panel{class=["pagination pagination-large pagination-centered"],body=[
+      #list{body=[
+        #li{class=["disabled"],body=#link{text="Prev"}},
+        #li{class=["active"], body=#link{text="1"}},
+        #li{body=#link{text="2"}},
+        #li{body=#link{text="3"}},
+        #li{body=#link{text="4"}},
+        #li{body=#link{text="5"}},
+        #li{body=#link{text="6"}},
+        #li{body=#link{text="Next"}}
+      ]}
+    ]},
+    #hr{},
+    #footer{class=["text-center"],body=#panel{body=["&copy; synrc.com"]}}
+  ]} ].
 
 event(init) -> [];
 event(Event) -> error_logger:info_msg("Page event: ~p", [Event]),ok.
