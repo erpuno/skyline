e1ca81bac16550b8023958747c3c4a698f891379
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-05-24 18:12:21 +0000

    update uglifyjs script. create flat-ui.min.js

--- uglify_js.sh
+++ uglify_js.sh
@@ -2,36 +2,19 @@
 
 # npm install uglify-js -g
 
-# keep the order
-declare -a sc=(
-  bootstrap-affix.js.min
-  bootstrap-alert.js.min
-  bootstrap-button.js.min
-  bootstrap-carousel.js.min
-  bootstrap-collapse.js.min
-  bootstrap-dropdown.js.min
-  bootstrap-modal.js.min
-  bootstrap-tooltip.js.min
-  bootstrap-scrollspy.js.min
-  bootstrap-tab.js.min
-  bootstrap-transition.js.min
-  bootstrap-typeahead.js.min
-  bootstrap-popover.js.min
-);
+function create_script {
+  cd $1
+  args=()
+  for i in `ls -p -S | grep -v "/"`; do #sort hack to keep order
+    echo $i
+    args+=($(uglifyjs $i))
+  done
+  echo ${args[@]} > $2
+  cd -
+}
 
-cd deps/bootstrap/js
-for i in `ls -d * | grep js`; do
-  echo -n 'process' $i'...'
-  uglifyjs $i -o $i.min;
-  echo 'done'
-done
-
-rm -f bootstrap.min.js
-cat ${sc[@]} > bootstrap.min.js
-rm -r *.min
-cp bootstrap.min.js ../../../apps/web/priv/static/js/
-rm -f bootstrap.min.js
-cd ../../..
+create_script deps/bootstrap/js ../../../apps/web/priv/static/js/bootstrap.min.js
+create_script deps/flat-ui/js ../../../apps/web/priv/static/js/flat-ui.min.js
 
 
 
