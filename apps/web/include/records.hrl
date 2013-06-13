ef35cdd0f5a6ff08db2545966a21d9962820d85a
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-06-13 03:29:23 +0000

    tinymce html editor element

--- apps/web/include/records.hrl
+++ apps/web/include/records.hrl
@@ -1,4 +1,2 @@
--record(feed_entry,     {?ELEMENT_BASE(element_feed_entry), entry}).
--record(feed_comment,   {?ELEMENT_BASE(element_feed_comment), comment}).
--record(feed_media,     {?ELEMENT_BASE(element_feed_media), media, target, fid=0, cid=0, only_thumb}).
+-record(tinymce, {?ELEMENT_BASE(element_tinymce), html="", script_url="static/tinymce/tinymce.min.js", theme="modern"}).
 
