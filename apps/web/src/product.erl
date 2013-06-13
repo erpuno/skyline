22c7ce1b4b1a959211825c7e354f2b211f573213
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-06-13 03:26:59 +0000

    remove global product feed elements.

--- apps/web/src/product.erl
+++ apps/web/src/product.erl
@@ -7,6 +7,10 @@
 
 -define(PAGE_SIZE, 4).
 
+-record(feed_entry,     {?ELEMENT_BASE(product), entry}).
+-record(feed_comment,   {?ELEMENT_BASE(product), comment}).
+-record(feed_media,     {?ELEMENT_BASE(product), media, target, fid=0, cid=0, only_thumb}).
+
 
 main() -> #dtl{file="dev", bindings=[{title,<<"product">>},{body, body()}]}.
 
@@ -26,10 +30,75 @@ body() ->
 
 feed() -> [#feed_entry{entry=E} || E <- entries(), _I <- lists:seq(1, 5)].
 
+render_element(#feed_entry{entry=E, id=Id}) ->
+  {{Y, Mm, D}, {_H, _M, _S}} = E#entry.created,
+
+  Li = #li{class=[thumbnail, span12, clearfix, "product-review"], body=[
+    #panel{class=[span3, "review-author"], body=[
+      #link{class=[avatar], body=#image{class=["img-circle"], data_fields=[{<<"data-src">>, <<"holder.js/100x100">>}]}},
+      #p{body=[
+        #link{url="#", body=[ #i{class=["icon-user"]}, #span{body =E#entry.from}]},
+        #span{class=["review-date"],body=[#i{class=["icon-calendar"]}, io_lib:format("~p/~p/~p",[Mm, D, Y]) ]},
+        #link{url="#",body=[ #i{class=["icon-thumbs-up"]}, #span{class=["badge badge-info"], body= <<"3">>} ]},
+        #link{url="#",body=[ #i{class=["icon-comment"]}, #span{class=["badge badge-info"], body= <<"10">>} ]},
+        #link{url="#",body=[ #i{class=["icon-share"]}, #span{class=["badge badge-info"], body= <<"5">>} ]}
+      ]}
+    ]},
+
+    #panel{class=[span3], body=[#feed_media{media=M} || M <- E#entry.media]},
+
+    #panel{class=[span6], body=[
+      #h4{body = title()},
+      description(wf:temp_id(), E#entry.description)
+    ]},
+
+    #list{class=[comments],body=list_comments(Id)}
+  ]},
+  element_li:render_element(Li);
+render_element(_R = #feed_comment{comment=C}) ->
+  #li{body=[
+    #panel{body=#link{body= <<"comment">>}},
+    #p{body=C#comment.content},
+    #panel{class=[media], body=list_medias(C)}
+  ]};
+render_element(FeedMedia = #feed_media{media = Media}) ->
+  case Media#media.type of
+    image -> image_media(FeedMedia);
+    _ -> image_media(FeedMedia)
+  end.
+
+list_comments(_Eid)->
+  Comments =[],
+  [#feed_comment{comment=C}|| C <- Comments].
+
+title()-> <<"Review title">>.
+
+description(Id, Description) -> [
+  #panel{id="description"++Id, body=[
+    #panel{class=[collapse, in], body= <<"Description head">>},
+    #panel{id=Id, class=collapse, body= Description}
+  ]},
+  #button{class=[btn, "btn-link"], data_fields=[
+    {<<"data-toggle">>, <<"collapse">>},
+    {<<"data-target">>, list_to_binary("#"++Id) },
+    {<<"data-parent">>, list_to_binary("#description"++Id)}], body= <<"Read...">>}].
+
+list_medias(C)->
+  {Cid, Fid} = C#comment.id,
+  [#feed_media{media=M, target=wf:temp_id(), fid = Fid, cid = Cid} ||  M <- C#comment.media].
+
+image_media(_Media)-> element_image:render_element(#image{image= "/static/img/item-bg.png"}).
+
 event(init) -> [];
 event(Event) -> error_logger:info_msg("Page event: ~p", [Event]), [].
 
 api_event(Name,Tag,Term) -> error_logger:info_msg("Name ~p, Tag ~p, Term ~p",[Name,Tag,Term]), event(change_me).
+
+control_event(Id, Tag) -> 
+  error_logger:info_msg("Tinymce editor control event ~p: ~p", [Id, Tag]),
+  Ed = wf:q(mcecontent),
+  error_logger:info_msg("Data:  ~p", [Ed]),
+  ok.
 
 entries()-> [
   #entry{
