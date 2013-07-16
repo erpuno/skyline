eb7d8f02fd436a1cb209fde48db07ec1cd9b9f43
Maxim Sokhatsky <maxim@synrc.com>
2013-07-16 06:59:29 +0300

    correct js build script

--- javascript.sh
+++ javascript.sh
@@ -9,10 +9,10 @@ function create_script {
     echo $i
     args+=($(uglifyjs $i))
   done
+  cd -
   echo $2
   echo ${args[@]} > $2
-  cd -
 }
 
-create_script deps/n2o/priv/static/bootstrap/js ../../js/bootstrap.min.js "/"
-create_script deps/n2o/priv/static/n2o ../../../../../apps/web/priv/static/js/all.min.js "/|all.js|zepto.js"
+create_script deps/n2o/priv/static/bootstrap/js apps/web/priv/static/js/bootstrap.min.js "/"
+create_script deps/n2o/priv/static/n2o apps/web/priv/static/js/all.min.js "/|all.js|zepto.js"
