84317a6c78a8853eb771b31fdb55e01c93df7f44
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-07-23 01:18:56 +0000

    remove local tinymce element. add dev scripts

--- apps/web/include/records.hrl
+++ apps/web/include/records.hrl
@@ -1,4 +1,3 @@
--record(tinymce,        {?ELEMENT_BASE(element_tinymce), html="", script_url="static/tinymce/tinymce.min.js", theme="modern"}).
 -record(feed_entry,     {?ELEMENT_BASE(product), entry}).
 -record(feed_comment,   {?ELEMENT_BASE(product), comment}).
 -record(feed_media,     {?ELEMENT_BASE(product), media, target, fid=0, cid=0, only_thumb}).
