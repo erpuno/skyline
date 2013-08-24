f74aaf32f0489e6494606faa5baf8c62fbc3caba
Maxim Sokhatsky <maxim@synrc.com>
2013-08-24 13:25:10 +0300

    move header, footer and login to html

--- apps/web/src/index.erl
+++ apps/web/src/index.erl
@@ -10,126 +10,32 @@ main() ->
               Body = "Body",
               [ #dtl{file = "prod", ext="dtl", bindings=[{title,Title},{body,Body}]} ] end.
 
-body() -> %% area of http handler
-    {ok,Pid} = wf:comet(fun() -> chat_loop() end), 
-    wf:wire(#api{name=apiOne,tag=d1}),
-  [
-    #span { body= <<"Your chatroom name: ">> }, 
-    #textbox { id=userName, body= wf:user() }, #button{body="Logout",postback=logout}, #br{},
-    #panel { id=chatHistory },
-    #button{id=but,body= <<"Click Me!">>,postback=change_me},
-    #button{id=replace,body= <<"Replace Body">>,postback=replace},
-    "<a onclick=\"document.apiOne('Hello')\" name='1'>API</a>",
-    #textbox { id=message },
-    #button { id=sendButton, body= <<"Chat">>, postback={chat,Pid}, source=[userName,message] },
-    #panel { id=n2ostatus }
- ].
+body() -> [].
 
-header()-> header(false).
-header(Inverse) -> [
-  #panel{class=[navbar, "navbar-fixed-top", if Inverse==true->"navbar-inverse"; true-> "" end, "sky-navbar"], body=[
-    #panel{class=["navbar-inner"], body=[
-      #panel{class=[container], body=[
-        #link{class=[btn, "btn-navbar"], data_fields=[{<<"data-toggle">>, <<"collapse">>}, {<<"data-target">>, <<".nav-collapse">>}], url="javascript:void(0)",
-          body=[#span{class=["icon-bar"]}||_I<-lists:seq(1,3)]},
+account() ->
+    #li{body=[
+        case wf:user() of
+             undefined -> #link{id=login1, body= <<"Log in">>, postback=to_login, delegate=login};
+             User -> #link{class=["dropdown-toggle", "avatar"], url="/account", body=[
+                           case User#user.avatar of 
+                                undefined -> "";
+                                Img -> #image{class=["img-circle", "img-polaroid"], 
+                                              image=iolist_to_binary([Img,"?sz=50&width=50&height=50&s=50"]), 
+                                              width= <<"50px">>, height= <<"50px">>} end,
+                           case User#user.display_name of undefined -> []; N -> N end]} end,
+        #button{id="style-switcher", class=[btn, "btn-inverse", "dropdown-toggle", "account-link"], 
+                data_fields=[{<<"data-toggle">>, <<"dropdown">>}], body=#i{class=["icon-cog"]}},
+        #list{class=["dropdown-menu"], body=[
+            #li{body=#link{body=[#i{class=["icon-cog"]},  <<" Preferences">>]}},
+            #li{body=#link{postback=chat,body=[#i{class=["icon-cog"]},  <<" Notifications">>]}},
+            case wf:user() of
+                 undefined -> #li{body=#link{id=loginbtn, postback=to_login, delegate=login, 
+                                             body=[#i{class=["icon-off"]}, <<" Login">> ]}};
+                 _A -> #li{body=#link{id=logoutbtn, postback=logout, delegate=login, 
+                                      body=[#i{class=["icon-off"]}, <<" Logout">> ] }} end ]} ]}.
 
-        #h1{class=[brand], body=#link{url="/login", body= <<"Synrc App Store">>, name="top" }},
-        #panel{class=["nav-collapse", "collapse"], body=[
-          #list{class=[nav, "pull-right"], body=[
-            #li{body=#link{url="/chat",body=[ #i{class=["icon-comment"]}, #span{class=["badge badge-info"], body="10"} ]}},
-            #li{body=#link{url="/chat?mode=mail",body=[ #i{class=["icon-envelope"]}, #span{class=["badge badge-info"], body="21"} ]} },
-            #li{body=#link{body=[ #i{class=["icon-search"]} ]}},
-            #li{body=#link{body= <<"Home">>,url="/login"}},
-            #li{body=#link{body= <<"Games">>,url="/products"}},
-            #li{body=#link{body= <<"Review">>}},
-            #li{body=[
-              case wf:user() of
-                undefined -> #link{id=login1, body= <<"Log in">>, postback=to_login, delegate=login};
-                User -> #link{class=["dropdown-toggle", "avatar"],
-                    %data_fields=[{<<"data-toggle">>, <<"dropdown">>}],
-                    url="/account", body=[
-                    case User#user.avatar of undefined-> ""; Img-> #image{class=["img-circle", "img-polaroid"], image=iolist_to_binary([Img,"?sz=50&width=50&height=50&s=50"]), width= <<"50px">>, height= <<"50px">>} end,
-                    case User#user.display_name of undefined -> []; N -> N end]} end,
-              #button{id="style-switcher", class=[btn, "btn-inverse", "dropdown-toggle", "account-link"], data_fields=[{<<"data-toggle">>, <<"dropdown">>}], body=#i{class=["icon-cog"]}},
-              #list{class=["dropdown-menu"], body=[
-                #li{body=#link{body=[#i{class=["icon-cog"]},  <<" Preferences">>]}},
-                #li{body=#link{postback=chat,body=[#i{class=["icon-cog"]},  <<" Notifications">>]}},
-                case wf:user() of
-                  undefined -> #li{body=#link{id=loginbtn, postback=to_login, delegate=login, body=[#i{class=["icon-off"]}, <<" Login">> ]}};
-                  _A -> #li{body=#link{id=logoutbtn, postback=logout, delegate=login, body=[#i{class=["icon-off"]}, <<" Logout">> ] }} end ]} ]} ]} ]} ]} ]} ]} ].
-
-footer()-> [
-  #footer{id=mainfooter, class=[section, "sky-footer"], body=
-    #panel{class=["row-fluid"], body=[
-          #panel{class=[span5, "footer-banner"], body=[
-            #h3{body= <<"Synrc Research Center">>},
-            #p{body = <<"Feel free to share your thoughts on Voxoz, Erlang, PaaS and other things.">>},
-            #list{class=[icons], body=[
-              #li{body=[#i{class=["icon-github", "icon-2x"]}, #link{url= <<"https://github.com/synrc">>, body= <<"Synrc Repositories">>}]}
-            ]},
-            #list{class=[unstyled], body=[
-              #li{body= <<" &copy; 2013 Synrc Research Center s.r.o.">>},
-              #li{body= <<" Roh&#225;&#269;ova 141/18, Praha 3 13000, Czech Republic">>},
-              #li{body= <<" HQ: Chokolivsky blvd, 19A, off. 8, Kyiv, Ukraine">>}
-            ]}
-          ]},
-          #panel{class=[span2], body=[
-            #h3{body= <<"voxoz">>},
-            #list{class=[unstyled], body=[
-              #li{body=#link{url= <<"http://voxoz.com">>, body= <<"How it works">>}},
-              #li{body=#link{url= <<"http://voxoz.com/pricing.html">>, body= <<"Pricing">>}},
-              #li{body=#link{url= <<"http://voxoz.com">>, body= <<"Applications">>}}
-            ]}
-          ]},
-          #panel{class=[span5], body=[
-            #h3{body= <<"Recent news">>},
-            #list{class=[unstyled], body=[
-              #li{body=[#h3{body=[#link{url= <<"http://voxoz.com/">>, body= <<"First Erlang PaaS">>}]}, #p{body= <<"Jun 12, 2013">>}]},
-              #li{body=[#h3{body=[#link{url= <<"http://synrc.com/framework/web">>, body= <<"N2O: Fastest Erlang Web Framework">>}]}, #p{body= <<"May 1, 2013">>}]} ]} ]} ]} }].
+header() -> [ #dtl{file = "hd", ext="dtl", bindings=[{account,account()}]} ].
+footer() -> [ #dtl{file = "tl", ext="dtl", bindings=[]} ].
 
 api_event(Name,Tag,Term) -> error_logger:info_msg("Index Name ~p, Tag ~p, Term ~p",[Name,Tag,Term]), event(change_me).
-
-event(init) ->
-  User = wf:user(),
-   error_logger:info_msg("User: ~p",[User]),
-  [ begin
-          Terms = [ #span { body= [[208,188,208,176,208,186,209,129,208,184,208,188]] }, ": ",
-                      #span { body=integer_to_list(N) }, #br{} ],
-            wf:insert_bottom(chatHistory, Terms)
-            end || N <- lists:seq(1,3) ];
-
-event(change_me) ->
-    wf:replace(but,
-        #link{
-            url= <<"http://erlang.org">>,
-            body= <<"Here's Erlang">>,
-            actions=#show{effect=fade}
-        }
-    );
-
-event(replace) ->
-    wf:wire(#redirect{url="hello",nodrop=false});
-
-event({chat,Pid}) -> %% area of websocket handler
-    error_logger:info_msg("Chat Pid: ~p",[Pid]),
-    Username = wf:user(),
-    Message = wf:q(message),
-    Terms = [ #span { body= <<"Message sent">> }, #br{} ],
-    wf:insert_bottom(chatHistory, Terms),
-    wf:wire("$('#message').focus(); $('#message').select(); "),
-    wf:reg(room),
-    Pid ! {message, Username, Message};
-
 event(Event) -> error_logger:info_msg("Event: ~p", [Event]).
-
-chat_loop() -> %% background worker ala comet
-    receive 
-        {message, Username, Message} ->
-            Terms = [ #span { body=Username }, ": ",
-                      #span { body=Message }, #br{} ],
-            wf:insert_bottom(chatHistory, Terms),
-            wf:wire("$('#chatHistory').scrollTop = $('#chatHistory').scrollHeight;"),
-            wf:flush(room); %% we flush to websocket process by key
-        Unknown -> error_logger:info_msg("Unknown Looper Message ~p",[Unknown])
-    end,
-    chat_loop().
--- apps/web/src/login.erl
+++ apps/web/src/login.erl
@@ -6,45 +6,11 @@
 
 main() ->
   avz:callbacks(?METHODS),
-  [#dtl{file = "prod",  ext="dtl",bindings=[{title,<<"Login">>},{body, body()}]} ].
-
-body() ->
-  index:header() ++ [
-  #panel{id="content", role="main", class=["theme-pattern-lightmesh", alt], body=[
-  #section{class=[section], id=promo, body=[
-  #panel{class=[container], body=[
-    #panel{class=[modal, "modal-login"], body=[ 
-      #panel{class=["form-horizontal"], id=loginform, body=[
-        #panel{class=["modal-header"], body=[
-          #button{class=[close], data_fields=[{<<"data-dismiss">>,<<"modal">>}], 
-                                 aria_states=[{<<"aria-hidden">>, <<"true">>}], body= <<"&times;">>},
-          #h3{body= <<"Log in to your account">>} ]},
-        #panel{class=["modal-body"], body=[
-          #panel{id=messages, body=[]},
-          #h3{class=["text-center"], body= <<"Sign in with">>},
-          #panel{class=["btn-toolbar", "text-center"], body=[
-            avz:buttons(?METHODS) ]},
-          #h3{class=["text-center"], body= <<"or">>},
-          #panel{class=["control-group"], body=[
-            #label{class=["control-label"], for=user, body= <<"Email">>},
-            #panel{class=[controls], body=[
-              #panel{class=["input-prepend"], body=[
-                #span{class=["add-on"], body=#i{class=["icon-user"]}},
-                #textbox{id=user, placeholder= <<"e-mail">>, data_fields=[{<<"data-original-title">>, <<"">>}]} ]} ]} ]},
-          #panel{class=["control-group"], body=[
-            #label{class=["control-label"], for=pass, body= <<"Password">>},
-            #panel{class=[controls], body=[
-              #panel{class=["input-prepend"], body=[
-                #span{class=["add-on"], body=#i{class=["icon-lock"]}},
-                #password{id=pass, data_fields=[{<<"data-original-title">>, <<"">>}]} ]} ]} ]},
-          #panel{class=["control-group"], body=[
-            #panel{class=[controls], body=[
-              #checkbox{id=remember, class=["checkbox"], checked=checked, body="Remember me"} ]} ]} ]},
-        #panel{class=["modal-footer"], body=[
-          #link{class=["pull-left", "link-forgot"], body= <<"forgot password?">>},
-          #button{id=login, class=[btn, "btn-info", "btn-large"], body= <<"Sign in">>, postback=login, source=[user,pass]} ]} ]} ]},
-    #panel{class=["pull-center"], body=[<<"Not a member?">>, #link{body= <<" Sign Up">>} ]} ]} ]} ]} ] 
-    ++ index:footer() ++ avz:sdk(?METHODS) .
+  [ #dtl{file = "login",  ext="dtl",bindings=[{title,<<"Login">>},
+                                              {header,index:header()},
+                                              {footer,index:footer()},
+                                              {sdk,avz:sdk(?METHODS)},
+                                              {buttons,avz:buttons(?METHODS)}]} ].
 
 event(X) -> avz:event(X).
 api_event(X,Y,Z) -> avz:api_event(X,Y,Z).
