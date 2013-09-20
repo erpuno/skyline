56ee6d88e8d4e488f038e94ef0aec3ae418c2065
Flex <flex@FlexBook-Pro.local>
2013-09-20 14:47:37 +0200

    More BSD support in shell scripts. Though release_sync.sh still relies on bash.

--- styles.sh
+++ styles.sh
@@ -3,4 +3,6 @@
 APPSTORE=apps/web/priv/static/appstore
 CSS=apps/web/priv/static/css
 
-/usr/bin/node apps/web/priv/static/less/bin/lessc -x $APPSTORE/appstore.less > $CSS/appstore.css
+NODE_APP=/usr/bin/node
+
+$NODE_APP apps/web/priv/static/less/bin/lessc -x $APPSTORE/appstore.less > $CSS/appstore.css
