9a4f5c5de24fd0531d09149e865aba1a13e17af5
Maxim Sokhatsky <maxim@synrc.com>
2013-09-20 11:02:32 +0300

    bsd support for javascript.sh, thanks to @fleximus

--- javascript.sh
+++ javascript.sh
@@ -1,18 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 
-# npm install uglify-js -g
+uglifyjs=`which uglifyjs`
+uglifijs_args='-nc'
 
-function create_script {
-  cd $1
-  args=()
-  for i in `ls -p -S *.js | egrep -v $3`; do #sort hack to keep order
-    echo $i
-    args+=($(uglifyjs $i))
-  done
-  cd -
-  echo $2
-  echo ${args[@]} > $2
+check_uglifyjs() {
+        if [ "$uglifyjs" == "" ] ; then
+                echo "ERROR: uglifyjs not found. Please set uglifyjs in javascript.sh"
+                exit 1;
+        fi
 }
+
+create_script() {
+        output="`pwd`/$2"
+        echo "Generating `basename $output` in `dirname $output`"
+        cp /dev/null $output
+
+        cd $1
+
+        # sort hack to keep order:
+        for js in `ls -p -S *.js | egrep -v $3`; do
+                echo "-> $js"
+                $uglifyjs $uglifyjs_args $js >> $output
+        done
+
+        echo "done."
+        cd -
+}
+
+check_uglifyjs
 
 create_script deps/n2o/priv/static/bootstrap/js apps/web/priv/static/js/bootstrap.min.js "/"
 create_script deps/n2o/priv/static/n2o apps/web/priv/static/js/all.min.js "/|all.js|zepto.js"
