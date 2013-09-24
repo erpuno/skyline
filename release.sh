6c8e3db86e012f4e84e04e528d181e649d3b8a76
Namdak Tonpa <maxim@synrc.com>
2013-09-24 15:25:30 +0000

    release fix, thanks to @krestenkrab

--- release.sh
+++ release.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
 NODE=${1:-"web"}
-ERLANG=/usr/lib/erlang
-
-if [ "$OSTYPE" == "FreeBSD" ] ; then
-    ERLANG=/usr/local/lib/erlang
-fi
+ERLANG_LIB=$(erl -noshell -eval 'io:format("~s", [code:lib_dir()]).' -s erlang halt)
 
 release_node() {
     rm -rf rels/$1/node/lib
     rm -rf rels/$1/node/log
     rm -rf rels/$1/node/releases
     (cd rels/$1; rebar -f generate)
-    cp -R $ERLANG/lib/erl_interface-* rels/$1/node/lib/
+    cp -R $ERLANG_LIB/erl_interface-* rels/$1/node/lib/
     install -d rels/$1/node/kai
 }
 
--- styles.sh
+++ styles.sh
@@ -3,6 +3,6 @@
 APPSTORE=apps/web/priv/static/appstore
 CSS=apps/web/priv/static/css
 
-NODE_APP=/usr/bin/node
+NODE_APP=node
 
 $NODE_APP apps/web/priv/static/less/bin/lessc -x $APPSTORE/appstore.less > $CSS/appstore.css
