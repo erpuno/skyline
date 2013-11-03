359b81faf2009e70b60dedfd1ccc722332c2ad3f
Maxim Sokhatsky <maxim@synrc.com>
2013-11-03 21:29:49 +0200

    mix

---
+++ mix.exs
@@ -0,0 +1,5 @@
+defmodule Sample.Mixfile do
+  use Mix.Project
+  def project do [ app: :skyline, version: "1.0.0", deps: deps ] end
+  defp deps do [ {:n2o_elixir, github: "synrc/n2o_elixir"} ] end
+end
