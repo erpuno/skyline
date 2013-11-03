6dc6c16c4180f799a639e7f3d83ef854412519af
Maxim Sokhatsky <maxim@synrc.com>
2013-11-03 13:56:19 +0200

    added relx support

--- synrc.mk
+++ synrc.mk
@@ -7,25 +7,21 @@ RUN_DIR ?= ./rels/web/devbox
 LOG_DIR ?= ./rels/web/devbox/logs
 
 deps: get-deps
-
 get-deps compile clean:
 	$(REBAR) $@
-
 console:  .applist
 	ERL_LIBS=$(ERL_LIBS) erl $(ERL_ARGS) -eval '[ok = application:ensure_started(A, permanent) || A <- $(shell cat .applist)]'
-
 .applist:
 	./envgen.erl $(APPS) > $@t
-
 $(RUN_DIR) $(LOG_DIR):
 	mkdir -p $(RUN_DIR)
 	mkdir -p $(LOG_DIR)
 	mkdir -p kai
-
 start: $(RUN_DIR) $(LOG_DIR)
 	run_erl -daemon $(RUN_DIR)/ $(LOG_DIR)/ "exec $(MAKE) console"
-
 attach:
 	to_erl $(RUN_DIR)/
+release:
+	relx
 
 .PHONY: get-deps compile clean deps console start attach
