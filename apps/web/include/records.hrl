cbfda3f0476ed37ebeecc65596f0ee6af2dfc671
Maxim Sokhatsky <maxim@synrc.com>
2013-11-05 22:59:39 +0200

    kvs_rest concept

--- apps/web/include/records.hrl
+++
@@ -1,4 +0,0 @@
--record(feed_entry,     {?ELEMENT_BASE(product), entry}).
--record(feed_comment,   {?ELEMENT_BASE(product), comment}).
--record(feed_media,     {?ELEMENT_BASE(product), media, target, fid=0, cid=0, only_thumb}).
--record(product_figure, {?ELEMENT_BASE(product), product}).
---
+++ apps/web/src/kvs_rest.erl
@@ -0,0 +1,20 @@
+-module(kvs_rest).
+-compile(export_all).
+-include_lib("n2o/include/wf.hrl").
+-include_lib("kvs/include/products.hrl").
+-include_lib("kvs/include/users.hrl").
+
+?rest().
+
+constructor(Resource) ->
+    case Resource of 
+         user -> #user{};
+         product -> #product{} end.
+
+init()            -> ok.
+get(Resource,[])    -> kvs:all(Resource);
+get(Resource,Id)    -> kvs:get(Resource,Id).
+delete(Resource,Id) -> kvs:remove(Resource,Id).
+put(Resource,User)  -> kvs:put(User).
+exists(Resource,Id) -> kvs:get(Resource,Id) /= [].
+post(Resource,Data) -> kvs:add((wf:to_atom("kvs_"++wf:to_list(Resource))):unmap(Resource,Data,constructor(Resource))).
